7a757ffe811e84ca48f49a1c35a895e59b0dc4c1
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-09-12 22:14:55 -0400

    three updates
    Created web app draft,
    added keymap reminder,
    changed background

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,9 +13,52 @@ import kotlinx.serialization.Serializable
 
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
+    document.bgColor = "honeydew"
     createRoot(container).render(Fragment.create {
         h1 {
             +"Hello, React+Kotlin/JS!"
         }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            p {
+                +"John Doe: Building and breaking things"
+            }
+            p {
+                +"Jane Smith: The development process"
+            }
+            p {
+                +"Matt Miller: The Web 7.0"
+            }
+            h3 {
+                +"Videos watched"
+            }
+            p {
+                +"Tom Jerry: Mouseless development"
+            }
+        }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+            }
+        }
+
     })
 }
+
+
+/*
+The render() function instructs kotlin-react-dom to render the first HTML element inside a
+fragment to the root element. This element is a container defined in src/main/resources/index
+.html, which was included in the template.
+
+The content is an <h1> header and uses a typesafe DSL to render HTML.
+
+h1 is a function that takes a lambda parameter. When you add the + sign in front of a string
+literal, the unaryPlus() function is actually invoked  using operator overloading. It appends the
+ string to the enclosed HTML element.
+*/
