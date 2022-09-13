9eaa38a5a21e5c2cbfb9cf2a428237e35242e6b8
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-09-12 23:56:41 -0400

    Added videos and styles

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,6 +11,27 @@ import react.dom.html.ReactHTML.img
 import react.dom.client.createRoot
 import kotlinx.serialization.Serializable
 
+
+data class Video(
+    val id: Int,
+    val title: String,
+    val speaker: String,
+    val videoUrl: String
+)
+val unwatchedVideos = listOf(
+    Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+    Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+    Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+)
+
+val watchedVideos = listOf(
+    Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+)
+
+
+
+
+
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
     document.bgColor = "honeydew"
@@ -22,23 +43,26 @@ fun main() {
             h3 {
                 +"Videos to watch"
             }
-            p {
-                +"John Doe: Building and breaking things"
-            }
-            p {
-                +"Jane Smith: The development process"
-            }
-            p {
-                +"Matt Miller: The Web 7.0"
+            for (video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
             h3 {
                 +"Videos watched"
             }
-            p {
-                +"Tom Jerry: Mouseless development"
+            for (video in watchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
         }
         div {
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
             h3 {
                 +"John Doe: Building and breaking things"
             }
