0f3576c1fc0f63d54df8f66bdeb3eda4f0efaf20
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-09-13 22:38:25 -0400

    Additional Components and Comments
    Extracted video player, added button, passed handlers and added comments.

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -15,8 +15,20 @@ import react.dom.html.ReactHTML.p
 import react.dom.html.ReactHTML.img
 
 val App = FC<Props> {
+  /*
+  The three lists are now in application state, which is the parent.
+  */
     var currentVideo: Video? by useState(null)
-    //Adding currentVideo as a state to the App component. We're lifting state.
+    var unwatchedVideos: List<Video> by useState(listOf(
+        Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+        Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+        Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+    ))
+    var watchedVideos: List<Video> by useState(listOf(
+        Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+    ))
+
+
     document.bgColor = "honeydew"  //Changing background color. Works in either App or Main
 
     h1 {
@@ -29,32 +41,41 @@ val App = FC<Props> {
         VideoList {
             videos = unwatchedVideos
             selectedVideo = currentVideo
-            onSelectVideo = { video -> currentVideo = video
+            onSelectVideo = { video ->
+                currentVideo = video
+            }
+            h3 {
+                +"Videos watched"
+            }
+            VideoList {
+                videos = watchedVideos
+                selectedVideo = currentVideo
+                onSelectVideo = { video ->
+                    currentVideo = video
+                }
+            }
         }
-        h3 {
-            +"Videos watched"
-        }
-        VideoList {
-            videos = watchedVideos
-            selectedVideo = currentVideo
-            onSelectVideo = {
-                video -> currentVideo = video
+
+        /*
+        The VideoPlayerProps interface specifies that the VideoPlayer comp takes a non-null video,
+        we make sure to handle this within the App.
+        The let scope func ensures that the VideoPlayer comp is only added when state.currentVideo isn't null.
+        Clicking an entry brings up the video player and populates it.
+        */
+        currentVideo?.let { curr ->
+            VideoPlayer {
+                video = curr
+                unwatchedVideo = curr in unwatchedVideos
+                onWatchedButtonPressed = {
+                    if (video in unwatchedVideos) {
+                        unwatchedVideos = unwatchedVideos - video
+                        watchedVideos = watchedVideos + video
+                    } else {
+                        watchedVideos = watchedVideos - video
+                        unwatchedVideos = unwatchedVideos + video
+                    }
+                }
             }
         }
     }
-    div {
-        css {
-            position = Position.absolute
-            top = 10.px
-            right = 10.px
-        }
-        h3 {
-            +"John Doe: Building and breaking things"
-        }
-        img {
-            src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-        }
-    }
-
-}
 }
